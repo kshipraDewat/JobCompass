272e636771d5c7d84c20055a8294225bdfe58c31
kshipraDewat <kshipradewat66@gmail.com>
2024-10-29 08:58:41 +0530

    Shadcn ui setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css'
+import { Button } from "@/components/ui/button"
 
 function App() {
 
   return (
-    <div className=' text-blue-700 text-2xl'>
+    <div className=' text-blue-700 text-2xl p-10'>
       Job Compass
+      <Button className="mx-2">Click me</Button>
+      
     </div>
   )
 }
