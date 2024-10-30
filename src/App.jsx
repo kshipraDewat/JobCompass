05a433d80c9226d7f95773a7a41d9defd295d8bc
kshipraDewat <kshipradewat66@gmail.com>
2024-10-30 08:01:24 +0530

    Routes setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,56 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './App.css'
 import { Button } from "@/components/ui/button"
+import AppLayout from './layout/AppLayout'
+import Landing from './pages/Landing'
+import Onboarding from './pages/Onboarding'
+import SavedJobs from './pages/SavedJobs'
+import PostJobs from './pages/PostJobs'
+import JobListing from './pages/JobListing'
+import MyJobs from './pages/MyJobs'
+import Jobs from './pages/Jobs'
+
+const router  = createBrowserRouter([
+  {
+    element:<AppLayout/>,
+    children:[
+      {
+        path:'/',
+        element: <Landing/>
+      },
+      {
+        path:'/onboarding',
+        element: <Onboarding/>
+      },
+      {
+        path:'/jobs',
+        element: <Jobs/>
+      },
+      {
+        path:'/jobs/:id',
+        element: <Jobs/>
+      },
+      {
+        path:'/postjobs',
+        element: <PostJobs/>
+      },
+      {
+        path:'/savedjobs',
+        element: <SavedJobs/>
+      },
+      {
+        path:'/myjobs',
+        element: <MyJobs/>
+      }
+    ]
+  }
+])
 
 function App() {
 
   return (
-    <div className=' text-blue-700 text-2xl p-10'>
-      Job Compass
-      <Button className="mx-2">Click me</Button>
+    <div className=' text-2xl '>
+      <RouterProvider router={router}/>
       
     </div>
   )
