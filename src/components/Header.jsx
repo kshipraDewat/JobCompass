5068e8500f608e156923e7020e6cf064323264bf
kshipraDewat <kshipradewat66@gmail.com>
2024-10-31 09:00:15 +0530

    supabase and clerk  setup + connection

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,16 +2,27 @@ import React from 'react'
 import logo from '../assets/logoo.png'
 import { Link } from 'react-router-dom'
 import { Button } from './ui/button'
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
 
 const Header = () => {
   return (
     <div>
-      <nav className='py-4 flex justify-between items-center'>
+      <nav className='py-4 px-5 flex justify-between items-center'>
         <Link>
         <img src={logo} alt="" className='size-14'/>
         </Link>
 
-        <Button variant='outline' className='' >Login</Button>
+        
+        <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton/>
+      </SignedIn>
+       
+       
+       
+
       </nav>
        
     </div>
